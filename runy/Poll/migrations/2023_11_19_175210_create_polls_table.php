011e3a2fa3aa0d80511ca3695066be47b1efb516
امین فراست <amin.ferasat@gmail.com>
2023-11-27 07:43:30 +0330

    000-00-7.72

--- runy/Poll/migrations/2023_11_19_175210_create_polls_table.php
+++ runy/Poll/migrations/2023_11_19_175210_create_polls_table.php
@@ -6,22 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
+
     public function up(): void
     {
         Schema::create('polls', function (Blueprint $table) {
             $table->id();
             $table->string('subject');
-            $table->boolean('status')->default(0);
+            $table->string('poll_type');
+            $table->string('status')->default(0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+
     public function down(): void
     {
         Schema::dropIfExists('polls');
