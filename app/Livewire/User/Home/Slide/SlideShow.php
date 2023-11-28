d9b736fdb964c78e6b5ff0c41f5428f62d05e973
امین فراست <amin.ferasat@gmail.com>
2023-11-28 06:56:52 +0330

    000-00-7.9 fix slide

--- app/Livewire/User/Home/Slide/SlideShow.php
+++ app/Livewire/User/Home/Slide/SlideShow.php
@@ -16,7 +16,7 @@ class SlideShow extends Component
 
     public function render()
     {
-        //$this->pics = RunySliderPics::query()->where('runy_slider_id' , $this->homePage->slideshow_id)->get();
+        $this->pics = RunySliderPics::query()->where('runy_slider_id' , $this->homePage->slideshow_id)->get();
         return view('livewire.user.home.slide.slide-show');
     }
 }
--- resources/views/livewire/user/home/slide/slide-show.blade.php
+++ resources/views/livewire/user/home/slide/slide-show.blade.php
@@ -1,36 +1,36 @@
 <div class="container-fluid p-0">
     <div class="row">
-{{--        <div id="carouselHomePage" class="carousel slide">--}}
-{{--            <div class="carousel-indicators">--}}
-{{--                @php $x=0; @endphp--}}
-{{--                @foreach($pics as $pic)--}}
-{{--                    <button type="button" data-bs-target="#carouselHomePage" data-bs-slide-to="{{ $x }}"--}}
-{{--                            @if($x == 1 ) class="active" @endif--}}
-{{--                            aria-current="true" aria-label="Slide {{ $x++ }}"></button>--}}
+        <div id="carouselHomePage" class="carousel slide">
+            <div class="carousel-indicators">
+                @php $x=0; @endphp
+                @foreach($pics as $pic)
+                    <button type="button" data-bs-target="#carouselHomePage" data-bs-slide-to="{{ $x }}"
+                            @if($x == 1 ) class="active" @endif
+                            aria-current="true" aria-label="Slide {{ $x++ }}"></button>
 
-{{--                @endforeach--}}
-{{--            </div>--}}
-{{--            <div class="carousel-inner">--}}
-{{--                @php $y=0; @endphp--}}
-{{--                @foreach($pics as $pic)--}}
-{{--                    <div class="carousel-item @if($y == 0) active @endif ">--}}
-{{--                        <img src="{{ asset($pic->urlpic) }}" class="d-block w-100" alt="{{ $pic->name }}">--}}
-{{--                        <div class="carousel-caption d-none d-md-block">--}}
-{{--                            {{$pic->text}}--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    @php $y++ @endphp--}}
-{{--                @endforeach--}}
-{{--            </div>--}}
-{{--            <button class="carousel-control-prev" type="button" data-bs-target="#carouselHomePage" data-bs-slide="prev">--}}
-{{--                <span class="carousel-control-prev-icon" aria-hidden="true"></span>--}}
-{{--                <span class="visually-hidden">قبلی</span>--}}
-{{--            </button>--}}
-{{--            <button class="carousel-control-next" type="button" data-bs-target="#carouselHomePage" data-bs-slide="next">--}}
-{{--                <span class="carousel-control-next-icon" aria-hidden="true"></span>--}}
-{{--                <span class="visually-hidden">بعدی</span>--}}
-{{--            </button>--}}
-{{--        </div>--}}
+                @endforeach
+            </div>
+            <div class="carousel-inner">
+                @php $y=0; @endphp
+                @foreach($pics as $pic)
+                    <div class="carousel-item @if($y == 0) active @endif ">
+                        <img src="{{ asset($pic->urlpic) }}" class="d-block w-100" alt="{{ $pic->name }}">
+                        <div class="carousel-caption d-none d-md-block">
+                            {{$pic->text}}
+                        </div>
+                    </div>
+                    @php $y++ @endphp
+                @endforeach
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselHomePage" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">قبلی</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselHomePage" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">بعدی</span>
+            </button>
+        </div>
         <!-- =======End slider ======= -->
     </div>
 </div>
