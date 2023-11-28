4266390aa3988c7e27ea35d4d81ac0bd74d35abc
امین فراست <amin.ferasat@gmail.com>
2023-11-28 11:34:24 +0330

    000-00-8.3 Update poll_show_route.php

--- runy/Poll/poll_show_route.php
+++ runy/Poll/poll_show_route.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use Poll\Controllers\PollController;
 
 
-Route::prefix('/poll')->middleware('auth')->group( function (){
+Route::prefix('/poll')->group( function (){
     Route::get('/show/{id}', [PollController::class , 'show'])->name('poll.show');
 
 });
