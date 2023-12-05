30f896e5eba319b04dcdbe7fc8b7eeb19486a15c
امین فراست <amin.ferasat@gmail.com>
2023-12-05 09:10:59 +0330

    Update poll_show_route.php 2

--- runy/Poll/poll_show_route.php
+++ runy/Poll/poll_show_route.php
@@ -5,6 +5,9 @@ use Poll\Controllers\PollController;
 
 
 Route::prefix('/poll')->group( function (){
+    Route::get('/show/12', function (){
+        return redirect('https://mortazavistore.ir/poll/show/15');
+    });
     Route::get('/show/{id}', [PollController::class , 'show'])->name('poll.show');
 });
 
