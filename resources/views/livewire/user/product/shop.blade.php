5bf36083188e74783ae67e5dac7836c1cfdcf208
امین فراست <amin.ferasat@gmail.com>
2023-12-23 00:16:30 +0330

    000-01-3.04

--- resources/views/livewire/user/product/shop.blade.php
+++ resources/views/livewire/user/product/shop.blade.php
@@ -6,24 +6,41 @@
     </div>
     <div class="row">
         <!-- Blog entries-->
+        @if(strlen($search) == 0 )
+        <div class="col-md-8 m-auto mb-5">
+            <h1 class="text-danger text-center font-family-Lalezar-Regular" itemprop="name">فروشگاه ماشین های اداری مرتضوی</h1>
+            <h2 class="text-danger text-center" >نمایندگی Brother در اصفهان</h2>
+            <p class="text-muted py-3 text-center">با گارانتی اصالت کالا </p>
+            <form class="custom-form" role="search">
+                <div class="input-group input-group-lg mb-2">
+                    <input wire:model="search" name="keyword" type="search" class="form-control border-color-runy-danger" id="keyword"
+                           placeholder="دنبال چه دستگاهی میگردی؟" aria-label="Search">
+
+                    <button type="submit" class="btn btn-runy-search bg-white" wire:click.prevent="searchBtn()">
+                        <svg class="icon-svg-panel" viewBox="0 0 24 24" fill="none"
+                             xmlns="http://www.w3.org/2000/svg">
+                            <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                            <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                            <g id="SVGRepo_iconCarrier">
+                                <path
+                                    d="M11 6C13.7614 6 16 8.23858 16 11M16.6588 16.6549L21 21M19 11C19 15.4183 15.4183 19 11 19C6.58172 19 3 15.4183 3 11C3 6.58172 6.58172 3 11 3C15.4183 3 19 6.58172 19 11Z"
+                                    stroke="#fd5858" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round"></path>
+                            </g>
+                        </svg>
+                    </button>
+                </div>
+                پیشنهاد : <span class="small-suggest p-1 border rounded border-color-runy-danger-badge mt-2">پرینتر لیزری برادر</span>
+                ، <span
+                    class="small-suggest p-1 border rounded border-color-runy-danger-badge mt-2">پرینتر چند کاره Brother</span>
+                <span></span>
+            </form>
+        </div>
+        @endif
         <div class="col-md-9">
             @if(strlen($search) > 1 )
                 <div class=" d-lg-flex justify-content-between bg-light py-3 px-4 job-item align-items-center mb-4 ">
-                    <p>جستجو برای  : <strong>{{ $search }}</strong> </p>
-                </div>
-            @else
-                <div class=" d-lg-flex justify-content-between bg-light py-3 px-4 job-item align-items-center mb-4 ">
-
-                    <div class="d-flex btn-group col-3 p-2 rounded text-danger">
-                        <label class="visually-hidden" for="autoSizingSelect">مرتب سازی بر اساس</label>
-                        <select class="form-select" id="autoSizingSelect">
-                            <option selected>با کیفیت ترین</option>
-                            <option value="1">گران ترین</option>
-                            <option value="2">ارزان ترین</option>
-                            <option value="3">جدید ترین</option>
-                        </select>
-                    </div>
-                    <a href="" class="btn btn-primary  mt-3 mt-lg-0">مرتب سازی بر اساس</a>
+                    <p>جستجو برای : <strong>{{ $search }}</strong></p>
                 </div>
             @endif
 
@@ -75,8 +92,10 @@
                 <div class="card-header">جستجوی محصول</div>
                 <div class="card-body">
                     <div class="input-group">
-                        <input class="form-control" type="text" wire:model="search" placeholder="نام محصول مورد نظر خود را بنویسید"/>
-                        <button class="btn btn-primary" id="button-search" wire:click.prevent="searchBtn()">جستجو</button>
+                        <input class="form-control" type="text" wire:model="search"
+                               placeholder="نام محصول مورد نظر خود را بنویسید"/>
+                        <button class="btn btn-primary" id="button-search" wire:click.prevent="searchBtn()">جستجو
+                        </button>
                     </div>
                 </div>
             </div>
