4ab046a9e80c75d9832470e41ac608a705e1d505
امین فراست <amin.ferasat@gmail.com>
2023-12-27 07:11:01 +0330

    000-01-3.55

---
+++ app/Livewire/User/Theme/Menu/MegaSubMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\User\Theme\Menu;
+
+use Livewire\Component;
+
+class MegaSubMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.user.theme.menu.mega-sub-menu');
+    }
+}
---
+++ resources/views/livewire/user/theme/menu/mega-sub-menu.blade.php
@@ -0,0 +1,359 @@
+<ul class="sub-menu nav">
+    <li class="list-item list-item-has-children">
+        <i class="now-ui-icons arrows-1_minimal-left"></i>
+        <a class="main-list-item nav-link" href="{{ asset('/shop') }}">همه محصولات</a>
+    </li>
+    <li class="list-item list-item-has-children">
+        <i class="now-ui-icons arrows-1_minimal-left"></i>
+        <a class="main-list-item nav-link">
+            ماشین های اداری
+        </a>
+
+        <ul class="sub-menu nav">
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i>
+                <a href="https://mortazavistore.ir/product-category/printer" class="main-list-item nav-link">پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="https://mortazavistore.ir/product-category/Laser-printer">پرینتر لیزری</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="https://mortazavistore.ir/product-category/Ink-jet-printer">پرینتر جوهر افشان</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="https://mortazavistore.ir/product-category/Photo-printer">پرینتر چاپ عکس</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="https://mortazavistore.ir/product-category/dot-matrix-printer">پرینتر سوزنی</a>
+            </li>
+
+            <li class="list-item list-item-has-children">
+                <i class="now-ui-icons arrows-1_minimal-left"></i>
+                <a class="main-list-item nav-link" href="https://mortazavistore.ir/product-category/scaner">اسکنر</a>
+            </li>
+
+            <li class="list-item list-item-has-children">
+                <i class="now-ui-icons arrows-1_minimal-left"></i>
+                <a class="main-list-item nav-link" href="https://mortazavistore.ir/product-category/%D9%84%DB%8C%D8%A8%D9%84-%D8%B2%D9%86">لیبل زن</a>
+                <ul class="sub-menu nav">
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Label-printer-accessories">لوازم جانبی لیبل پرینتر</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Manual-labeler">لیبل زن دستی</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="list-item list-item-has-children">
+                <i class="now-ui-icons arrows-1_minimal-left"></i>
+                <a class="main-list-item nav-link" href="https://mortazavistore.ir/product-category/accessories">لوازم جانبی</a>
+                <ul class="sub-menu nav">
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/chipset">چیپست</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/drama">درام</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Printer-head">هد پرینتر</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/adapter">آداپتور</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/blade">بلید</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/magnet">مگنت</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/foam-roller">فوم رولر</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/fusing">فیوزینگ</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Fusing-movie">فیلم فیوزینگ</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Foam-meter-board">برد فومتر</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/power-board">برد پاور</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Belt-transfer">ترنسفر بلت</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Pres">پرس</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/hot-roll">هات رول</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/paper-cutter">کاغذ کش</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Suppression-Pad">سپریشن پد</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/output-tray">سینی خروجی</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/input-tray">سینی ورودی</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/pterygium">ناخنک</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/gear">چرخ دنده</a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="https://mortazavistore.ir/product-category/Other-parts">سایر قطعات</a>
+                    </li>
+                </ul>
+            </li>
+
+            <li class="list-item list-item-has-children">
+                <i class="now-ui-icons arrows-1_minimal-left"></i>
+                <a class="main-list-item nav-link" href="#"></a>
+                <ul class="sub-menu nav">
+                    <li class="list-item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                    <li class="list-item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                </ul>
+            </li>
+
+
+        </ul>
+    </li>
+    <li class="list-item list-item-has-children">
+        <i class="now-ui-icons arrows-1_minimal-left"></i>
+        <a class="main-list-item nav-link" href="#">تجهیزات فروشگاهی و
+            بانکی</a>
+        <ul class="sub-menu nav">
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i><a
+                    class="main-list-item nav-link">بارکدخوان</a>
+
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">بارکدخوان با سیم</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">بارکدخوان بی سیم</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">بارکدخوان همراه</a>
+            </li>
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i><a
+                    class="main-list-item nav-link">لیبل پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">لوازم جانبی لیبل پرینتر</a>
+            </li>
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i><a
+                    class="main-list-item nav-link">فیش پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">فیش پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">صندوق فروشگاهی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">نرم افزار فروش و حسابداری</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">لیبل زن دستی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">موبایل پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کشوی پول</a>
+            </li>
+        </ul>
+    </li>
+    <li class="list-item list-item-has-children">
+        <i class="now-ui-icons arrows-1_minimal-left"></i><a class="nav-link" href="#">مواد
+            مصرفی</a>
+        <ul class="sub-menu nav">
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i><a
+                    class="main-list-item nav-link">کارتریج</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کارتریج لیزری رنگی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کارتریج لیزری مشکی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کارتریج جوهرافشان</a>
+            </li>
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i><a
+                    class="main-list-item nav-link">ریبون</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">ریبون پرینتر سوزنی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">ریبون لیبل پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">ریبون کارت پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">ریبون تاریخ زن</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">تونر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">لیبل (برچسب)</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">نوار لیبل پرینتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کارت pvc</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> رول حرارتی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">جوهر پرینتر و پلاتر </a>
+            </li>
+
+
+        </ul>
+    </li>
+    <li class="list-item list-item-has-children">
+
+
+        <ul class="sub-menu nav">
+            <li class="list-item">
+                <a class="nav-link" href="#">تانک و کارتریج قابل شارژ </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کاغذ عکس </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">دولوپر</a>
+            </li>
+
+
+            <li class="list-item">
+                <a class="nav-link" href="#">مرکب ریسوگراف</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">باتری</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">کاربن فکس</a>
+            </li>
+            <li class="list-item">
+                <i class="now-ui-icons arrows-1_minimal-left"></i><a
+                    class="main-list-item nav-link">قطعات</a>
+            </li>
+
+            <li class="list-item">
+
+                <a class="nav-link" href="#">چیپست</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">درام</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">بلید </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> مگنت </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">فرم رولر </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> فیوزینگ</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> فیلم فیوزینگ</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> برد فرمتر</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> برد پاور</a>
+            </li>
+
+
+        </ul>
+    </li>
+    <li class="list-item list-item-has-children">
+
+        <ul class="sub-menu nav">
+            <li class="list-item">
+                <a class="nav-link" href="#"> ترنسفر بلت</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> پرس</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> هات رول</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> کاغذ کش</a>
+            </li>
+
+
+            <li class="list-item">
+                <a class="nav-link" href="#"> سپریشن پد </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> سینی خروجی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> سینی ورودی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> ناخنک </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> چرخ دنده </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> هد پرینتر </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> آداپتور </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> سایر قطعات </a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#"> تعمیر دستگاه کپی</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">تعمیر پرینترهای اپسون</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">تعمیر دستگاه فیش زن</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">تعمیر دستگاه بارکدخوان</a>
+            </li>
+            <li class="list-item">
+                <a class="nav-link" href="#">شارژ کارتریج سامسونگ</a>
+            </li>
+        </ul>
+
+    </li>
+</ul>
